d215128f32081af750c142db18da74905d8baedb
Mathias Munene <munenemathias@gmail.com>
2025-05-13 00:49:38 +0300

    todo function added among others

--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -1,10 +1,20 @@
 import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
+import {v4 as uuidv4} from 'uuid';
+uuidv4();
 
 export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+      setTodos([...todos, {id: uuidv4(), tals: todo,
+        completed: false, isEditing: false        
+      }])
+      console.log(todos)
+    }
   return (
     <div className='ToDoWrapper'>
-      <TodoForm/>
+      <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
