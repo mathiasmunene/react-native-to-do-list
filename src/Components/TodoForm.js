05f66474299f03a7b5cdf9036aa10a180b1e7523
Mathias Munene <munenemathias@gmail.com>
2025-05-13 00:52:51 +0300

    setvalue() added

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -7,10 +7,12 @@ export const TodoForm = ({addTodo}) => {
       e.preventDefault();
 
       addTodo(value)
+
+      setValue("")
     }
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
-      <input type='text' className='todo-input'
+      <input type='text' className='todo-input' value={value}
       placeholder='What is the task today?' onChange=
       {(e) => setValue(e.target.value) }/>
       <button type='submit' className='todo-btn'>Add Task</button>
